e6525a76b0dbd2d817103d0c172e10e74962c94a
SebbePrime <sebber06@elev.danderyd.se>
2024-10-17 10:08:54 +0200

    V.12 (update and draw + other)

--- GYA.py
+++ GYA.py
@@ -6,8 +6,27 @@ Current Version v.12
 
 ## IMPORTS
 import random
-import pygame
+import pgzrun
+import os
 
 ## CODE
 game = True
 print("game")
+
+# OTHER-FUNKTIONS
+def other_funktions():
+    pass
+
+# FUNKTIONS
+def draw():
+    pass 
+
+def update():
+    pass
+
+## END-CODE
+
+#SCREEN
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+pgzrun.go()
+
